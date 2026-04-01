331e7afee7b865fbabb2a4e309a19ce35c0ce733
troy <menardtroyf@gmail.com>
2026-04-01 22:26:59 +0800

    Added QR Code Generator

--- Form1.vb
+++ Form1.vb
@@ -5,4 +5,19 @@ Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         connect()
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim input = TextBox1.Text
+            Using qrGenerator As New QRCodeGenerator()
+                Using qrCodeData = qrGenerator.CreateQrCode(input, QRCodeGenerator.ECCLevel.Q)
+                    Using qrCode As New QRCode(qrCodeData)
+                        PictureBox1.Image = qrCode.GetGraphic(20)
+                    End Using
+                End Using
+            End Using
+        Catch ex As Exception
+            PictureBox1.Image = Nothing
+        End Try
+    End Sub
 End Class
